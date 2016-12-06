da2de7262582989cac28259e87d9ef593cf49757
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-12-06 01:16:02 -0500

    more :car:  and other updates

---
+++ game/Assets/_Lvl2 Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    //Public
+    public NavMeshAgent Agent;
+    public Light Headlight;
+
+    //private
+    private Transform Player;
+    private Transform _CurrentEnemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.Player = GameObject.FindWithTag("Player").transform;
+        this._CurrentEnemy = this.GetComponent<Transform>();
+        Headlight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                //this.Agent.SetDestination(this.Player.position);
+    }
+}
---
+++ game/Assets/_Lvl2 Scripts/Lvl2GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl2GameController : MonoBehaviour {
+
+    public AudioSource backgroundmusic;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
