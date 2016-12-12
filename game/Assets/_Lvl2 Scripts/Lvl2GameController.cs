a60f27f3ee1135dd31826a8f7c5d637a44c7bdf0
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-12-12 16:26:35 -0500

    Issue #58 , #55 and fixed some bugs

--- game/Assets/_Lvl2 Scripts/Lvl2GameController.cs
+++ game/Assets/_Lvl2 Scripts/Lvl2GameController.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 public class Lvl2GameController : MonoBehaviour {
     // Private
     private GameObject[] _lights;
-    private WaitForSeconds _waitTime = new WaitForSeconds(5.0f);
     private bool _gamejuststart;
+    private bool _instruction;
+    private bool _warning;
 
     //public
     public AudioSource backgroundmusic;
@@ -25,9 +26,12 @@ public class Lvl2GameController : MonoBehaviour {
         }
         this.Player.transform.position = this.SpawnPoint.position;
         _gamejuststart = true;
+        _instruction = false;
         DisplayText.gameObject.SetActive(true);
         DisplayText.text = "Turn on the Lights";
-        StartCoroutine(_removeDisplay());
+        StartCoroutine(_removeDisplay(new WaitForSeconds(3.0f)));
+        StartCoroutine(_removeInstruction(new WaitForSeconds(5.0f)));
+        StartCoroutine(_removeWarning(new WaitForSeconds(7.0f)));
     }
 	
 	// Update is called once per frame
@@ -35,14 +39,36 @@ public class Lvl2GameController : MonoBehaviour {
         if (_gamejuststart)
         {
             DisplayText.gameObject.SetActive(true);
-            DisplayText.text = "Turn on the Lights";
+            DisplayText.text = "Find Your Car";
+        }
+        else if(_instruction)
+        {
+            DisplayText.gameObject.SetActive(true);
+            DisplayText.text = "Turn On the Lights";
+        }
+        else if(_warning)
+        {
+            DisplayText.gameObject.SetActive(true);
+            DisplayText.text = "Don't get hit by Enemies";
         }
     }
 
-    private IEnumerator _removeDisplay()
+    private IEnumerator _removeDisplay(WaitForSeconds _waitTime)
     {
-        yield return this._waitTime;
+        yield return _waitTime;
         _gamejuststart = false;
-        DisplayText.gameObject.SetActive(false);
+        _instruction = true;
+
+    }
+    private IEnumerator _removeInstruction(WaitForSeconds _waitTime)
+    {
+        yield return _waitTime;
+        _instruction = false;
+        _warning = true;
+    }
+    private IEnumerator _removeWarning(WaitForSeconds _waitTime)
+    {
+        yield return _waitTime;
+        _warning = false;
     }
 }
