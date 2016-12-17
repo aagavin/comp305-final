7d77547d7f55d73cb3e8e20187a0f632bdb71b80
Aaron <aagavin@gmail.com>
2016-12-16 20:14:53 -0500

    added cheats

--- game/Assets/_Scripts/MenuScript.cs
+++ game/Assets/_Scripts/MenuScript.cs
@@ -17,17 +17,17 @@ using UnityEngine.UI;
 /// Used for switching the menu scene to other levels
 /// </summary>
 public class MenuScript : MonoBehaviour {
-    // PUBLIC VARIABLES
-    public Button Button;
+	// PUBLIC VARIABLES
+	public Button Button;
 	/************** PUBLIC FUNCTIONS **************/
 
 	/// <summary>
 	/// Goto Level 1 Scene
 	/// </summary>
 	public void level1button_Click()
-    {
-        SceneManager.LoadScene("Level1");
-    }
+	{
+		SceneManager.LoadScene("Level1");
+	}
 
 	/// <summary>
 	/// Goto Level 2 Scene
@@ -48,17 +48,33 @@ public class MenuScript : MonoBehaviour {
 	/// <summary>
 	/// Goto Instructions scene
 	/// </summary>
-    public void instruction_Click()
-    {
-        SceneManager.LoadScene("Instructions");
-    }
+	public void instruction_Click()
+	{
+		SceneManager.LoadScene("Instructions");
+	}
 
 
 	/// <summary>
 	/// Goto menu Scene
 	/// </summary>
-    public void exit_Click()
-    {
-        SceneManager.LoadScene("Menu");
-    }
+	public void exit_Click()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+
+
+	void Update() {
+		//cheat for going to level various levels
+		if (Input.GetKeyDown ("1")) {
+			this.level1button_Click ();
+		} else if (Input.GetKeyDown ("2")) {
+			this.level2button_Click ();
+		} else if (Input.GetKeyDown ("3")) {
+			this.level3button_Click ();
+		}
+
+
+
+
+	}
 }
