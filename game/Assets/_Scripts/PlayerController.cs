1cab41438855a9cb6c8f1f4972c2d49523995eff
Waynell Lovell <wlovell2@my.centennialcollege.ca>
2016-12-16 20:35:04 -0500

    /play trololo Added score :checkered_flag:

--- game/Assets/_Scripts/PlayerController.cs
+++ game/Assets/_Scripts/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     public Transform PlayerDirection;
     public Transform Respawn;
     public Text DisplayText;
+    public Text ScoreText;
     public AudioSource PhoneCall;
     public AudioSource Sit;
     public AudioSource GetinCar;
@@ -30,12 +31,16 @@ public class PlayerController : MonoBehaviour
     // Private
     private GameObject[] _lights;
     private bool _isLightsOn;
+    private int _score;
 
     // Use this for initialization
     void Start()
     {
         _lights = GameObject.FindGameObjectsWithTag("Light");
         _isLightsOn = true;
+        _score=PlayerPrefs.GetInt("HighScore");
+        ScoreText.text = "Score: " + _score;
+        StartCoroutine(_addscore(new WaitForSeconds(1f)));
     }
 
     // FixedUpdate is called once per frame
@@ -101,6 +106,7 @@ public class PlayerController : MonoBehaviour
                 else if (look.transform.gameObject.CompareTag("Player'sCar"))
                 {
                     GetinCar.Play();
+                    PlayerPrefs.SetInt("HighScore", _score);
                     StartCoroutine(_moveToLevel3(new WaitForSeconds(5f)));
                 }
                 else if (look.transform.gameObject.CompareTag("Power"))
@@ -125,11 +131,20 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("boarder") || other.gameObject.CompareTag("Enemy"))
         {
             PlayerDirection.position = Respawn.position;
+            _score -= 20;
+            ScoreText.text = "Score: " + _score;
         }
     }
     private IEnumerator _moveToLevel3(WaitForSeconds _waitTime)
     {
         yield return _waitTime;
         SceneManager.LoadScene("Level2toLevel3");
+    }
+    private IEnumerator _addscore(WaitForSeconds _waitTime)
+    {
+        yield return _waitTime;
+        _score -= 1;
+        ScoreText.text = "Score: " + _score;
+        StartCoroutine(_addscore(new WaitForSeconds(1f)));
     }
 }
